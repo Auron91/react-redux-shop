ee59e74fa79a0e45b6e870920c11d523f30b05e0
Michał Bociek <mic.bociek@gmail.com>
2021-04-06 19:54:37 +0200

    shopify graph ql start

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,15 @@
 import shoes from '../apis/shoes'
 
 export const fetchProducts = () => async dispatch => {
-    const response = await shoes.get('/shoes')
+    const response = await shoes.get('/admin/api/2021-01/products.json', { mode: 'cors'})
 
     dispatch({type: 'FETCH_PRODUCTS', payload: response.data})
 }
 
 export const fetchProduct = (id) => async dispatch => {
-    const response = await shoes.get(`/shoes/${id}`)
+    const response = await shoes.get(`/admin/api/2021-01/products/${id}.json`)
 
-    dispatch({type: 'FETCH_PRODUCTS', payload: response.data})
+    dispatch({type: 'FETCH_PRODUCTS', payload: response.products})
 }
 
 export const toggleView = (view) => {
